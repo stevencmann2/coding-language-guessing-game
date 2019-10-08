45531d2fe9f9e63ac8e12d1647c37464a4139f2a
Steven Mann <stevencmann2@gmail.com>
2019-10-08 17:06:11 -0500

    set arrays into singular object to be called later

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,14 +3,17 @@ let wins = 0;
 let losses = 0;
 let guessesLeft = 5;
 let pastGuessesArray = [];
-const computerLanguagesArray = ["javascript", "html", "python", "java", "c++"];
-const questionsArray = []
-    questionsArray[0]="What behavioral programming language is being used to display this message?"; 
-    questionsArray[1]="What programming language is used to make and design the layout of webpages?";
-    questionsArray[2]="What is an interpreted, object-oriented, high-level programming language with dynamic semantics?"; 
-    questionsArray[3]="Which language is a class-based, object-oriented promgramming language developed by Sun Microsystems in the 1990's?"; 
-    questionsArray[4]="Which language was originally designed to enhance the C language?";
+const triviaCouples = {
 
+    answersArray: ["javascript", "html", "python", "java", "c++"],
+
+    questionsArray: 
+    ["What behavioral programming language is being used to display this message?",
+    "What programming language is used to make and design the layout of webpages?",
+    "What is an interpreted, object-oriented, high-level programming language with dynamic semantics?",
+    "Which language is a class-based, object-oriented promgramming language developed by Sun Microsystems in the 1990's?",
+    "Which language was originally designed to enhance the C language?"]
+}
 
 //this holds the place in the HTML document //
 
@@ -20,19 +23,27 @@ const lossesText = document.getElementById("lossestext");
 const guessesLeftText = document.getElementById("guesseslefttext");
 const pastGuessesText = document.getElementById("pastguesstext");
 const vanishText = document.getElementById("vanish");
- 
 
-// Randomly chooses a choice from the options array. This is the Computer's guess. and will begin the game
-function gameOn (){
+
+
+//Randomly chooses a choice from the questions array and will begin the game//
+function gameOn() {
     
     let computerQuestion = questionsArray[Math.floor(Math.random() * questionsArray.length)];
     document.getElementById("gameontext").innerHTML = computerQuestion;
     console.log(computerQuestion);
 
+    
 
+
+
+    /*
+    
 }
+*/
 
- 
+
+
 
 
 
@@ -41,11 +52,11 @@ function gameOn (){
 
 //document.onkeyup = function (event) {
 
-    // Determines which key was pressed.
-    //let userGuess = String.fromCharCode(event.keyCode).toLowerCase()
-    
+// Determines which key was pressed.
+//let userGuess = String.fromCharCode(event.keyCode).toLowerCase()
 
-   // console.log(computerChoice);
+
+// console.log(computerChoice);
 
 //hint the player as to the answer
 

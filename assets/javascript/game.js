96a0b83b64066d8957cff53fb7b29729fad98bf4
Steven Mann <stevencmann2@gmail.com>
2019-10-09 10:07:21 -0500

    debugging array and function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,54 +15,15 @@ const attemptsLeftText = document.getElementById("attemptslefttext");
 const pastGuessesText = document.getElementById("pastguesstext");
 const vanishText = document.getElementById("vanish");
 
-
+let computerChoice = Math.floor(Math.random() * (answersArray.length));
+console.log(computerChoice);
 
 //Randomly chooses a choice from the questions array and will begin the game//
-function gameOn() {
+
     // need to inlcude onkey event
-    document.onkeyup = function (event);
-        let userGuess = event.key
-    
-    console.log(userGuess)
-    
-    
-    
-    
-    /*
-    
-    triviaCouples.onkeyup = function myScrip
-    let computerQuestion = questionsArray[Math.floor(Math.random() * questionsArray.length)];
-    document.getElementById("gameontext").innerHTML = computerQuestion;
-    console.log(computerQuestion);
-
-    
-
-// for this homework it doesnt need a lead in question, just ask them to guess the programming language your asking for//
-// try and make it hang man like//
+    console.log(answersArray);
+   
 
 
-
-
-
-
-
-
-
-//document.onkeyup = function (event) {
-
-// Determines which key was pressed.
-//let userGuess = String.fromCharCode(event.keyCode).toLowerCase()
-
-
-// console.log(computerChoice);
-
-//hint the player as to the answer
-
-//player should know how many characters the game is looking for
-
-
-//number of attemps left
-
-//wins and losses totals
-
-// game needs to restart */
+   /* 
+    console.log(computerChoice) function gameOn() {; */
